15f24f0037bce4f16cc85f1e031891d59dd6acda
Irfan Nurul Susilo <irfansusilo88@gmail.com>
2024-06-11 16:42:08 +0700

    esm

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import express from 'express';
 import path from 'node:path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import indexRouter from '@routes/index';
-import usersRouter from '@routes/users';
-import { processCSS } from './middlewares/postcss'
+// import indexRouter from '@routes/index';
+// import usersRouter from '@routes/users';
+import { processCSS } from './middlewares/postcss.js'
+import vine from '@vinejs/vine'
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve('views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -19,7 +21,9 @@ app.use(cookieParser());
 app.get('/css/:file', async (req, res) => {
   try {
     const fileName = req.params.file;
-    const filePath = path.join(__dirname, '../public/css', fileName);
+    // const filePath = path.join(__dirname, '../public/css', fileName);
+    const filePath = path.resolve('public', 'css', fileName);
+
     const css = await processCSS(filePath);
     res.set('Content-Type', 'text/css');
     res.send(css);
@@ -28,10 +32,45 @@ app.get('/css/:file', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', async (req, res) => {
+  try {
+    const schema = vine.object({
+      username: vine.string(),
+      email: vine.string().email(),
+      password: vine
+        .string()
+        .minLength(8)
+        .maxLength(32)
+        .confirmed()
+    })
+    
+    const data = {
+      username: 'virk',
+      email: 'virk@example.com',
+      password: 'secret',
+      password_confirmation: 'secret',
+    }
+    
+    const output = await vine.validate({
+      schema,
+      data
+    })
+    
+    
+
+    res.set('Content-Type', 'text/css');
+    res.send('');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.resolve('public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
